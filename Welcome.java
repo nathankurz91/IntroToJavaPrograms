af2d7a8b5200e099f8c0766eceb5cd659e80c26c
Nathan Kurz <natcom@Nathans-MacBook-Air.local>
2018-03-11 09:47:34 -0400

    Add Welcome commit

---
+++ Welcome.java
@@ -0,0 +1,11 @@
+/**
+ * Developer: Nathan Kurz
+ * 
+ * Classic Hello World program
+ */
+
+ public class Welcome {
+     public static void main(String[] args) {
+         System.out.println("Welcome to Java!!");
+     }
+ }
