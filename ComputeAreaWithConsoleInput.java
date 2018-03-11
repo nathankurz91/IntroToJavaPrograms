4e3f42589fd89b732343308cc72450af9ae5396a
Nathan Kurz <natcom@Nathans-MacBook-Air.local>
2018-03-11 10:06:01 -0400

    Add ComputeAreaWithConsoleInpute commit

---
+++ ComputeAreaWithConsoleInput.java
@@ -0,0 +1,28 @@
+/**
+ * Developer: Nathan Kurz
+ * 
+ * Compute the area of a circle with console input
+ */
+
+import java.util.Scanner;
+
+public class ComputeAreaWithConsoleInput {
+    public static void main(String[] args) {
+        
+        // Request input from user
+        System.out.println("\nEnter the radius:");
+
+        // Create scanner to read input
+        Scanner in = new Scanner(System.in);
+
+        // Store input from user
+        double radius = in.nextDouble();
+
+        // Compute area
+        double area = radius * radius;
+
+        System.out.println("The area of a circle with radius " + radius + " is: " + area + "\n");
+
+        // Compute results
+    }
+}
