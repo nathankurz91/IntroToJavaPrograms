d846862d7e19ea8a97ab5caf6f6a2b99d34e5cd5
Nathan Kurz <natcom@Nathans-MacBook-Air.local>
2018-03-11 09:55:41 -0400

    Add ComputeCircleArea commit

---
+++ ComputeCircleArea.java
@@ -0,0 +1,16 @@
+/**
+ * Developer: Nathan Kurz
+ * 
+ * Compute the area of a circle
+ */
+public class ComputeCircleArea {
+    public static void main(String[] args) {
+
+        double radius = 5;
+
+        // area of a circle is r^2
+        double area = radius * radius;
+
+        System.out.println("The area of a circle of raidus " + radius + " is: " + area);
+    }
+}
